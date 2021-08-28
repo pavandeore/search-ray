e5c8a5513b488cd9ca5d2057796b3a33e15a935d
Pawan Deore <pawan@dezyre.com>
2021-08-28 22:05:41 +0530

    both search algorithms implemented

---
+++ index.js
@@ -0,0 +1,38 @@
+function linearSearch(arr, key){
+    let results = []
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === key){
+            results.push(i)
+        }
+    }
+    // If results array is empty, return -1
+    if(!results){
+        return -1
+    }
+
+    return results
+}
+
+function binarySearch(sortedArray, key){
+    let start = 0;
+    let end = sortedArray.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+
+        if (sortedArray[middle] === key) {
+            // found the key
+            return middle;
+        } else if (sortedArray[middle] < key) {
+            // continue searching to the right
+            start = middle + 1;
+        } else {
+            // search searching to the left
+            end = middle - 1;
+        }
+    }
+	// key wasn't found
+    return -1;
+}
+
+module.exports = { linearSearch, binarySearch }
